526e8067fa027a84e787aa4c55c63ceaef86fa26
brendantfitzgerald <brendantfitzgerald@gmail.com>
2019-01-03 11:38:00 -0600

    Uploaded the PowerShell script

---
+++ RandomCrawlLS.ps1
@@ -0,0 +1,21 @@
+﻿cls
+cd "~"
+while(1){$list = ls
+$list
+$levels = 0
+
+$directories = ls -dir
+
+$randomDirectory = ($directories| get-random -count 1)
+$goback = Get-Random -Maximum 1 -Minimum 0
+
+Write-Host $randomDirectory
+
+if($goback -eq 1 -or $randomDirectory.Exists){
+cd $randomDirectory
+}
+else{cd "~"
+cd ../..
+}
+Start-Sleep (Get-Random -Maximum 10 -Minimum 0)
+}
